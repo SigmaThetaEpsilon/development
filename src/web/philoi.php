68c063ecd69489b458e54fed2220caa120ab3bdd
Matt Zirkle <matt.zirkle@outlook.com>
2018-10-13 07:35:15 -0500

    Add Philoi Fall 2018.

--- src/web/philoi.php
+++ src/web/philoi.php
@@ -9,6 +9,7 @@ $pageHtml = <<<HTML
     <p>The Philoi has traditionally been the alumni newsletter of Sigma Theta Epsilon. Starting in 2018, it is a newsletter not just for alumni brothers, but also for the active brothers.</p>
     <p>Here is a list of Philoi newsletters that have been published:</p>
     <ul>
+        <li><a href="/files/philoi/Philoi102018.pdf" target="_blank">October 2018</a></li>
         <li><a href="/files/philoi/Philoi082003.pdf" target="_blank">July/August 2003</a></li>
         <li><a href="/files/philoi/Philoi111950.pdf" target="_blank">November 1950</a></li>
     </ul>
