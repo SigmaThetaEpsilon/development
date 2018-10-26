f17940476cd536cada2996ced50dce791c988e83
Matt Zirkle <matt.zirkle@outlook.com>
2018-10-25 19:00:05 -0500

    Update link to actual store. Show HTML message for when sale ends.

--- dist/web/store.php
+++ dist/web/store.php
@@ -8,8 +8,10 @@ $pageHtml = <<<HTML
     <h3 class="card-header">Store</h3>
     <p>Sigma Theta Epsilon has partnered with <a href="https://undergroundshirts.com/" target="_blank">Underground Printing</a> to carry branded merchandise. We have worked with a designer to come up with clothing and other merchandise to show off your STE side like never before.</p>
     <p>We have arranged for 2-week ordering periods to collect orders, after which they will be processed as a bulk order. You can always check back here to see if we are in an ordering period or a processing period.</p>
-    <p>The ordering form is not live yet, but here is an <a href="https://pogo.undergroundshirts.com/collections/example-business-pogo-form" target="_blank">example order form site</a> so you can see what it will look like.</p>
+    <p>Here is a link to the ordering form: <a href="https://pogo.undergroundshirts.com/collections/sigma-theta-epsilon" target="_blank">https://pogo.undergroundshirts.com/collections/sigma-theta-epsilon</a></p>
+    <div id="storeCountdownContainer"><p>Sale ends on 11/9/2018 at midnight.</p></div>
 </section>
+
 HTML;
 
 Layout::renderHTML('Store', '/store.php', $pageHtml);
