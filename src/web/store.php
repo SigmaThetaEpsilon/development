e18c370f5fdb254338d643fee7b19537326cf11d
Matt Zirkle <matt.zirkle@outlook.com>
2018-11-11 20:36:42 -0600

    Update the store page to indicate no longer in a sale.

--- src/web/store.php
+++ src/web/store.php
@@ -9,7 +9,7 @@ $pageHtml = <<<HTML
     <p>Sigma Theta Epsilon has partnered with <a href="https://undergroundshirts.com/" target="_blank">Underground Printing</a> to carry branded merchandise. We have worked with a designer to come up with clothing and other merchandise to show off your STE side like never before.</p>
     <p>We have arranged for 2-week ordering periods to collect orders, after which they will be processed as a bulk order. You can always check back here to see if we are in an ordering period or a processing period.</p>
     <p>Here is a link to the ordering form: <a href="https://pogo.undergroundshirts.com/collections/sigma-theta-epsilon" target="_blank">https://pogo.undergroundshirts.com/collections/sigma-theta-epsilon</a></p>
-    <div id="storeCountdownContainer"><p>Sale ends on 11/9/2018 at midnight.</p></div>
+    <div id="storeCountdownContainer"><p>We are currently in a processing period. Check back later for our next sale.</p></div>
 </section>
 
 HTML;
