b5d8ed5fe30c4215620826d7c12fbd706ce3c050
Matt Zirkle <matt.zirkle@outlook.com>
2020-06-09 16:46:57 -0500

    Remove old files

--- dist/application/Controllers/ResourcesController.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// TODO - For resources that are both public-facing and behind the login (active, alumni, national board, etc.)
-
-?>
--- src/application/Views/Chapters/IndexView.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__ . "/../IView.php");
-
-// TODO - Transition to this later.
-
-?>
